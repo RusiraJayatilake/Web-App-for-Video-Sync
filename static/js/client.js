cea9ade5386409a85cdef8d382580699851de66a
rusira <rusirajayatilake96@gmail.com>
2023-08-08 01:54:29 +1200

    Timeline and sync with video

--- static/js/client.js
+++ static/js/client.js
@@ -23,6 +23,26 @@ function onYouTubeIframeAPIReady() {
           'onStateChange': onPlayerStateChange,
         }
     });
+
+    // Set up the timeline input element
+    timeline = document.getElementById('timeline');
+    setInterval(updateTimeLine, 1000); //update every second
+
+    function updateTimeLine(){
+      if(player){
+        const currentTime = player.getCurrentTime();
+        const duration = player.getDuration();
+        const percentage = (currentTime / duration) * 100;
+        timeline.value = percentage;
+      }
+    }
+
+    timeline.addEventListener('input', function () {
+      const percentage = timeline.value;
+      const duration = player.getDuration();
+      const seekTime = (percentage / 100) * duration;
+      player.seekTo(seekTime);
+    });
 }
 
 var done = false;
@@ -38,28 +58,6 @@ const socket = io('http://localhost:2000');
 // Adding event listeners
 document.getElementById('play-btn').addEventListener('click', playButton);
 document.getElementById('pause-btn').addEventListener('click', pauseButton);
-
-// const timeline = document.getElementById('timeline');
-
-// Update the timeline when the video is playing
-// function updateTimeline(){
-//     if(player){
-//         const currentTime = player.getCurrentTime();
-//         const duration = player.getDuration();
-//         const percentage = (currentTime / duration) * 100;
-//         timeline.value = percentage;
-//     }
-// }
-
-// Update video playback based on timeline seek
-// timeline.addEventListener('input', function(){
-//     if (player) {
-//         const percentage = timeline.value;
-//         const duration = player.getDuration();
-//         const seekTime = (percentage / 100) * duration;
-//         player.seekTo(seekTime, true);
-//       }
-// });
 
 
 // Define playButton function
@@ -82,6 +80,5 @@ socket.on('playEvent', function (data) {
     else if (data.action === 'pause') {
       player.pauseVideo();
     }
-    // updateTimeline();
 });
 
